c9b6ae7b4aaaec056eeb3f5e0765c713598a32c5
Saqib Jallal <sjallal88@gmail.com>
2020-11-08 19:30:55 +0530

    User model is complete.

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  fname: {
+    type: String,
+    required: true,
+  },
+  lname: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  about: {
+    type: String,
+  },
+});
+
+module.exports = User = mongoose.model("user", userSchema);
