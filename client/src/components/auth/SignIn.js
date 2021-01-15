2a882e5bc77ea1c680c10c2244a4e5f3962119b3
Saqib Jallal <sjallal88@gmail.com>
2021-01-15 19:22:58 +0530

    Updated the file architecture

--- client/src/components/auth/SignIn.js
+++ client/src/components/auth/SignIn.js
@@ -1,7 +1,6 @@
 import React, { Fragment, useState } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { setAlert } from "../../actions/alert";
 import { signIn } from "../../actions/auth";
 
 const SignIn = ({ signIn }) => {
